3afd238c5faab71690ec9f5f987cce549b8dc23e
Vinicius-3D <62802830+The-vinicius@users.noreply.github.com>
2021-04-18 20:39:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -31,4 +31,4 @@ async def read_play(play_id: int, db: Session= Depends(get_db)):
     return consulta.get_play(db=db, play_id=play_id)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, debug=True)
